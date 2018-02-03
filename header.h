d9e3400815e6db2c9e1ba76975fff016e4db01f3
sugacane <sugacane@users.noreply.github.com>
2018-02-03 17:01:54 -0400

    added header file

---
+++ header.h
@@ -0,0 +1,15 @@
+//something i typed up :/
+Class Something {
+	private:
+	int thing;
+	public:
+	Something() {
+		thing = 0;
+	}
+	int getThing() {
+		return thing;
+	}
+	void setThing(int t) {
+		thing = t;
+	}
+}
